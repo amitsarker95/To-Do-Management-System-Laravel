946a177797cdd3f06b5959cb6744981c7aa0b19d
amit <amitsarker95@gmail.com>
2025-08-20 23:30:20 +0600

    database migrations and table adedd

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,31 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ThemeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-
+// Redirect home to todos if logged in
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check()
+        ? redirect()->route('todos.index')
+        : view('welcome');
 });
 
+// Auth-protected routes
 Route::middleware('auth')->group(function () {
-    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
-    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    // Resourceful routes (CRUD) for todos, except "show"
+    Route::resource('todos', TodoController::class)->except(['show']);
 
-    Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
-    Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
+    // Extra Todo features
+    Route::patch('/todos/{todo}/toggle', [TodoController::class, 'toggle'])
+        ->name('todos.toggle');
+    Route::delete('/todos', [TodoController::class, 'clearAll'])
+        ->name('todos.clear');
 
-    Route::patch('/todos/{id}/toggle', [TodoController::class, 'toggle'])->name('todos.toggle');
-    Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
-
-    Route::delete('/todos', [TodoController::class, 'clearAll'])->name('todos.clear');
-
-    Route::post('/theme/toggle', [ThemeController::class, 'toggle'])->name('theme.toggle');
+    // Theme toggle (Light/Dark Mode with cookies)
+    Route::post('/theme/toggle', [ThemeController::class, 'toggle'])
+        ->name('theme.toggle');
 });
 
+// Laravel authentication routes (Breeze/UI)
 require __DIR__.'/auth.php';
