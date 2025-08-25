9641c5c22866b9767485cd3b67544d45d306ce6a
amit <amitsarker95@gmail.com>
2025-08-25 13:45:49 +0600

    added ajax routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,12 @@ Route::middleware('auth')->group(function () {
         ->name('theme.toggle');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/ajax/todos', [TodoController::class, 'ajaxList'])->name('ajax.todos.list');
+    Route::post('/ajax/todos', [TodoController::class, 'ajaxStore'])->name('ajax.todos.store');
+    Route::patch('/ajax/todos/{todo}', [TodoController::class, 'ajaxUpdate'])->name('ajax.todos.update');
+    Route::delete('/ajax/todos/{todo}', [TodoController::class, 'ajaxDelete'])->name('ajax.todos.delete');
+});
+
 // Laravel authentication routes (Breeze/UI)
 require __DIR__.'/auth.php';
